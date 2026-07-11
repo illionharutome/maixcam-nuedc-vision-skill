39c19fb3c76cf2155bcbe3e3ccc290b37741258a
Guessy <18326048676@163.com>
2026-07-11 10:48:04 +0800

    fix aim error demo maixvision entrypoint

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-"""Default MaixVision entry: safe red-dot/substitute spot detection."""
-
-from apps_atomic.app_laser_spot_detect import main
-
+from apps_atomic.app_aim_error_demo import main
 
 main()
